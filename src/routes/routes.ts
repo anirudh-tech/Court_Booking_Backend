d6c00f1877940d102af4c2fbfeb0b157f516adbb
Anirudh-tech <anirudhjagath2002@gmail.com>
2024-06-14 17:41:03 +0530

    feat: fetch-user added

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,18 +4,24 @@ import { verifyAdminToken } from "../utils/jwt/adminVerify/verifyAdminToken";
 import { sportController } from "../controllers/sportController";
 import { courtController } from "../controllers/courtController";
 import { bookingController } from "../controllers/bookingController";
+import { userController } from "../controllers/userController";
 
 export const routes = () => {
   const { adminLogin,userLogin } = loginController();
   const { addSport, editSport } = sportController();
   const { addCourt, editCourt } = courtController();
-  const {bookCourt} = bookingController();
+  const {bookCourt,userBookingList} = bookingController();
+  const {fetchData} = userController();
   const router = Router();
 
   //admin routes
   // router.route("/add-admin").post(addAdmin)
   router.route("/admin-login").post(adminLogin);
   router.route("/user-login").post(userLogin)
+
+  //user routes
+
+  router.route("/fetch-user").post(fetchData)
 
   //sport routes
   router.route("/add-sport").post(addSport);
@@ -26,7 +32,8 @@ export const routes = () => {
   router.route("/edit-court").patch(editCourt);
 
   //booking routes
-  router.route("book-court").post(bookCourt);
+  router.route("/book-court").post(bookCourt);
+  router.route("/user-bookings-list/:id").get(userBookingList)
 
   return router;
 };
