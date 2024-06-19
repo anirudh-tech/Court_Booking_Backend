6e2bfb899a32d08f928c0f600a4d14e8fcbd706c
Anirudh-tech <anirudhjagath2002@gmail.com>
2024-06-19 13:32:14 +0530

    refactor: cors url updated

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 
 const PORT: number = Number(process.env.PORT) || 3000;
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://lal-sports-academy-git-client-anirudhs-projects-6a2748bf.vercel.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
